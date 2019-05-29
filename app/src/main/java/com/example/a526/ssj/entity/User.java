975eca0b87ac70410fb9ebdd63578a620509c2a1
Obsob <1762629204@qq.com>
2019-05-29 16:08:00 +0800

    注册登录接口

--- app/src/main/java/com/example/a526/ssj/entity/User.java
+++ app/src/main/java/com/example/a526/ssj/entity/User.java
@@ -15,4 +15,48 @@ public class User {
     private String email;
 
     private Integer state = 1;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setState(Integer state)
+    {
+        this.state = state;
+    }
+
+    public Integer getState() {
+        return state;
+    }
 }
---
+++ app/src/main/java/com/example/a526/ssj/util/LogUtil.java
@@ -0,0 +1,143 @@
+package com.example.a526.ssj.util;
+
+import android.util.JsonReader;
+
+import com.example.a526.ssj.entity.User;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * Created by asus on 2019/5/29.
+ */
+
+public class LogUtil {
+    private String hostAndPort = "localhost:8080";
+    private String loginUrl = "/login";
+    private String registerUrl = "/rigister";
+
+    public boolean login(String email, String password, Map<String, Object> map)
+    {
+        try {
+            URL url = new URL(loginUrl);
+            String data = "email=" + email + "&password=" + password;
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+            // 设置连接
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("User-Agent","Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; KB974487)");
+            conn.setRequestProperty("Charset", "UTF-8");
+
+            // 建立连接
+            conn.connect();
+
+            // 输入数据
+            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+            dos.writeBytes(data);
+            dos.flush();
+            dos.close();
+
+            int code = conn.getResponseCode();
+            if(code == HttpURLConnection.HTTP_OK)
+            {
+                // 读取数据
+                StringBuffer sb=new StringBuffer();
+                String readLine;
+                BufferedReader responseReader=new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+                while((readLine=responseReader.readLine())!=null){
+                    sb.append(readLine).append("\n");
+                }
+                responseReader.close();
+                System.out.println(sb.toString());
+                JSONObject jsonObject = new JSONObject(sb.toString());
+                map.put("state", jsonObject.get("state"));
+                map.put("message", jsonObject.get("message"));
+                if(jsonObject.has("user"))
+                {
+                    JSONObject userObject = new JSONObject(jsonObject.getString("user"));
+                    User user = new User();
+                    user.setId(userObject.getInt("id"));
+                    user.setName(userObject.getString("name"));
+                    user.setPassword(userObject.getString("password"));
+                    user.setEmail(userObject.getString("email"));
+                    user.setState(userObject.getInt("state"));
+                    map.put("user", user);
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean register(String name, String password, String email, Map<String, Object> map)
+    {
+        try {
+            URL url = new URL(loginUrl);
+            String data = "name=" + name + "&password=" + password + "&email=" + email;
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+            // 设置连接
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("User-Agent","Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; KB974487)");
+            conn.setRequestProperty("Charset", "UTF-8");
+
+            // 建立连接
+            conn.connect();
+
+            // 输入数据
+            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+            dos.writeBytes(data);
+            dos.flush();
+            dos.close();
+
+            int code = conn.getResponseCode();
+            if(code == HttpURLConnection.HTTP_OK)
+            {
+                // 读取数据
+                StringBuffer sb=new StringBuffer();
+                String readLine;
+                BufferedReader responseReader=new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+                while((readLine=responseReader.readLine())!=null){
+                    sb.append(readLine).append("\n");
+                }
+                responseReader.close();
+                System.out.println(sb.toString());
+                JSONObject jsonObject = new JSONObject(sb.toString());
+                map.put("state", jsonObject.get("state"));
+                map.put("message", jsonObject.get("message"));
+                if(jsonObject.has("user"))
+                {
+                    JSONObject userObject = new JSONObject(jsonObject.getString("user"));
+                    User user = new User();
+                    user.setId(userObject.getInt("id"));
+                    user.setName(userObject.getString("name"));
+                    user.setPassword(userObject.getString("password"));
+                    user.setEmail(userObject.getString("email"));
+                    user.setState(userObject.getInt("state"));
+                    map.put("user", user);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
+// {"state":"success","message":"登陆成功","user":{"id":1,"name":"user001","password":"123456","email":"123@qq.com","state":1}}
