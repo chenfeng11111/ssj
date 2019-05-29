108df00240184a151b96ed7e355b6abe31c6e912
chenfeng11111 <1090268270@qq.com>
2019-05-29 11:25:53 +0800

    用户类

---
+++ app/src/main/java/com/example/a526/ssj/entity/User.java
@@ -0,0 +1,18 @@
+package com.example.a526.ssj.entity;
+
+/**
+ * Created by 10902 on 2019/5/29.
+ */
+
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String email;
+
+    private Integer state = 1;
+}
