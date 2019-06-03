dc8a6f0c621bfe05be1a37dc11569a21b792a338
Charile_Lu <Charile_Lu@users.noreply.github.com>
2019-06-03 11:36:21 +0800

    整合用户设置相关的界面

--- app/src/main/java/com/example/a526/ssj/mainactivity/MineFragment.java
+++ app/src/main/java/com/example/a526/ssj/mainactivity/MineFragment.java
@@ -1,11 +1,16 @@
 package com.example.a526.ssj.mainactivity;
 
-import android.annotation.SuppressLint;
-import android.graphics.Color;
+
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.Gravity;
+
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.example.a526.ssj.R;
 
 /**
  * Created by 10902 on 2019/5/28.
@@ -14,6 +19,32 @@ import android.widget.TextView;
 public class MineFragment extends Fragment {
 
     private TextView textView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.mine_fragment, container, false);
+        TextView txtViewMy = (TextView)rootView.findViewById(R.id.myinformation);
+        TextView txtViewchange = (TextView)rootView.findViewById(R.id.psdchange);
+        txtViewMy.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent();
+                intent.setClass(getContext(),myinformationActivity.class);
+                startActivity(intent);
+            }
+        });
+        txtViewchange.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent();
+                intent.setClass(getContext(),passwordchangeActivity.class);
+                startActivity(intent);
+            }
+        });
+        return rootView;
+    }
 
 
 }
