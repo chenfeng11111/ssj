85b3b81884dfe108071718443c2186f9dc1e3bfe
JYZ <1529131084@qq.com>
2019-06-03 14:11:44 +0800

    调试了创建笔记的功能，优化弹出的菜单栏

--- app/src/main/java/com/example/a526/ssj/lockactivity/LockActivity.java
+++ app/src/main/java/com/example/a526/ssj/lockactivity/LockActivity.java
@@ -74,6 +74,7 @@ public class LockActivity extends AppCompatActivity {
                              * **************************
                              *
                              */
+                            finish();
 
                         }
                     }
